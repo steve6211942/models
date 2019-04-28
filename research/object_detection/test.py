d381ff3ae8d90f60df1f126236d4af8cc37e433e
steve6211942 <steve6211942@gmail.com>
2019-04-28 11:46:51 +0800

    detect ok image not ok

---
+++ research/object_detection/test.py
@@ -0,0 +1,13 @@
+import requests
+import cv2
+import numpy as np
+
+url = "http://10.42.0.244:8080/shot.jpg"
+
+while True:
+	img_resp = requests.get(url)
+	img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+	img = cv2.imdecode(img_arr, -1)
+	cv2.imshow("AndroidCam", img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break;
