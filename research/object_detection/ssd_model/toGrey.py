38c18a950eccad85d92b4ba235e3e43f998a5a9d
steve6211942 <steve6211942@gmail.com>
2019-07-08 22:12:59 +0800

    gray up

--- research/object_detection/ssd_model/toGrey.py
+++ research/object_detection/ssd_model/toGrey.py
@@ -1,7 +1,6 @@
 import cv2
 
-
 for x in range(320):
-	image = cv2.imread('images/train_img/ok_' + str(x+1) + '.jpg')
+	image = cv2.imread('images/train_img/up_' + str(x+1) + '.jpg')
 	image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	cv2.imwrite('images/tmp/ok_' + str(x+1) + '.jpg', image_grey)
+	cv2.imwrite('images/tmp/up_' + str(x+1) + '.jpg', image_grey)
