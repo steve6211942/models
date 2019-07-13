63d2a159ecd000c5eaf18bd2430c8453ac9ffc04
steve6211942 <steve6211942@gmail.com>
2019-07-13 09:40:00 +0800

    add csv

--- research/object_detection/ssd_model/mat2csv.py
+++ research/object_detection/ssd_model/mat2csv.py
@@ -1,7 +1,8 @@
 import scipy.io
 import pandas as pd
 
-mat = scipy.io.loadmat('./training_dataset/training_data/annotations/Buffy_1.mat')
-mat = {k:v for k, v in mat.items() if k[0] != '_'}
-data = pd.DataFrame({k: pd.Series(v[0]) for k, v in mat.items()})
-data.to_csv("./training_dataset/training_data/annotation/Buffy_1.csv")
+for i in range(1,1446):
+	mat = scipy.io.loadmat('./training_dataset/training_data/annotations/VOC2010_'+str(i)+'.mat')
+	mat = {k:v for k, v in mat.items() if k[0] != '_'}
+	data = pd.DataFrame({k: pd.Series(v[0]) for k, v in mat.items()})
+	data.to_csv("./training_dataset/training_data/annotation/VOC2010_"+str(i)+".csv")
